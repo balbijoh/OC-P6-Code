83fd931803352812ed88652073504d6785dbc537
balbijoh <johanne.balbin@gmail.com>
2023-02-03 17:05:06 +0100

    Add authentification routes/controllers/model/token

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const utilisateurRoutes = require('./routes/utilisateur.js');
+
 mongoose.connect('mongodb+srv://balbijoh:V47geur0bmxJR8zY@cluster0.8m5swpi.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .catch(() => console.log('Connexion à MongoDB : échec'));
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//   next();
-// });
+app.use(express.json());
 
-//--- Route GET ------------------------------------------------------
-// app.use('/api/stuff', (req, res, next) => {
-//   const stuff = [
-//     {
-//     },
-//   ];
-//   res.status(200).json(stuff);
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/auth', utilisateurRoutes);
 
 module.exports = app;
---
+++ backend/controllers/utilisateur.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt');
+const jwtoken = require('jsonwebtoken');
+const Utilisateur = require('../models/Utilisateur.js');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const utilisateur = new Utilisateur({
+            email: req.body.email,
+            password: hash,
+        });
+        utilisateur.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+    Utilisateur.findOne({ email: req.body.email })
+    .then(utilisateur => {
+        if (utilisateur === null) {
+            res.status(401).json({ message: 'Utilisateur introuvable' });
+        } else {
+            bcrypt.compare(req.body.password, utilisateur.password)
+            .then(valid => {
+                if (!valid) {
+                    res.status(401).json({ message: 'Mot de passe incorrect' });
+                } else {
+                    res.status(200).json({ 
+                        userId: utilisateur._id,
+                        token: jwtoken.sign(
+                            { userId: utilisateur._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                };
+            })
+            .catch(error => res.status(500).json({ error }));
+        };
+    })
+    .catch(error => res.status(500).json({ error }));
+};
